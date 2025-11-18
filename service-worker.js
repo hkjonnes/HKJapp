42810777ed73fe2003ed6f83bf451a0174026760
hkjonnes <hope.kale@yahoo.com>
2025-11-18 08:48:26 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -19,3 +19,28 @@ self.addEventListener("fetch", (e) => {
     })
   );
 });
+const CACHE_NAME = "hkjapp-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/src/data/songs.json"
+];
+
+// Install SW
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch offline support
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
